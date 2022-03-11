15bd22f143a54f27c1c9a031e2e4b09dbb4507cc
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-11 17:57:51 -0600

    update time-block and ad color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,24 @@
 //make able to change text area and keep current changes
 //add save button and icon
 //make save button move in and out when cursor is over it to save
+var tasks = {};
 
 var today = moment().format('ddd MM/DD/YYYY');
 $("#currentDay").html(today);
 
-var time = moment().format('HH:MM:SS');
-$("#currentTime").html(time);
+//if current time <>= time block then add a class past prestent or future 
+
+  function updateColor () {
+var time = moment().hours 
+$(".time-block").each(function() {
+    var timeColor = $(this).attr("id")
+    if (timeColor < time) {
+        $(this).addClass(".past")        
+    } else if (timeColor === time) {
+        $(this).addClass(".present")
+    } else if (timeColor > time) {
+        $(this).addClass(".future")
+    };
+});
+  };
+  
