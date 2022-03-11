455fe39c378ce6b96f7702b56b1eb861ded641dd
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-11 12:58:41 -0600

    add timeblocks and textarea

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//make textarea persistent using local.storage
+//add date and time to jumbotron
+//add color to textarea
+//give textarea color
+//make textarea change color as due dates get closer
+//make able to change text area and keep current changes
+//add save button and icon
+//make save button move in and out when cursor is over it to save
