9c3b2d6fff50e70b2a1bb32f2fccd64092accad0
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-11 15:09:31 -0600

    add time to jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 //make textarea persistent using local.storage
-//add date and time to jumbotron
+//add time to jumbotron
 //add color to textarea
 //give textarea color
 //make textarea change color as due dates get closer
 //make able to change text area and keep current changes
 //add save button and icon
 //make save button move in and out when cursor is over it to save
+
+var today = moment().format('ddd MM/DD/YYYY');
+$("#currentDay").html(today);
+
+var time = moment().format('HH:MM:SS');
+$("#currentTime").html(time);
