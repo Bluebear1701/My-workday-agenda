4d14c4e8fdab1731b8abff3f19c8b6af93056883
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-03-12 09:15:42 -0600

    added time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-// the current date
-var today = moment().format('ddd MM/DD/YYYY');
+// the current date and time
+var today = 
+moment().format('ddd MM/DD/YYYY, hh:mm');
 $("#currentDay").html(today);
 
 //save button click listener
@@ -11,10 +12,16 @@ $(".saveBtn") .on("click", function(){
     
 });
 
+//get from local storage 
 $("#9 .description").val(localStorage.getItem("9"));
-
-
-
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
 
 
 //if current time <>= time block then add a class past, present or future 
@@ -34,7 +41,3 @@ $(".time-block").each(function() {
  updateColor();
 
 
-//get current time  
-//loop over time blocks
-//check the time and add the class
-//get from local storage 
